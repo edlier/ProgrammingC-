c6b1cb33ff8d4b8bc7eb99ee228f579f075424c4
edlier <co9452294522@gmail.com>
2016-03-08 15:40:43 +0800

    restructure

--- randomPassword/randomPassword/Form1.cs
+++ randomPassword/randomPassword/Form1.cs
@@ -18,8 +18,6 @@ namespace randomPassword
         public Form1()
         {
             InitializeComponent();
-
-
         }
         private int Next()
         {
@@ -41,32 +39,54 @@ namespace randomPassword
             int value = Next(max - min) + min;
             return value;
         }
-        public string GetRandomFirstTittle(int length)
+        //public string GetRandomFirstTittle(int length)
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        //    for (int i = 0; i < length; i++)
+        //    {
+        //        sb.Append(chars[this.Next(chars.Length - 1)]);
+        //    }
+        //    string Password = sb.ToString();
+        //    return Password;
+        //}
+        //public string GetRandomSendtoForth(int length)
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    char[] chars = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        //    for (int i = 0; i < length; i++)
+        //    {
+        //        sb.Append(chars[this.Next(chars.Length - 1)]);
+        //    }
+        //    string Password = sb.ToString();
+        //    return Password;
+        //}
+        //public string GetRandomSendFinalNum(int length)
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    char[] chars = "0123456789".ToCharArray();
+        //    for (int i = 0; i < length; i++)
+        //    {
+        //        sb.Append(chars[this.Next(chars.Length - 1)]);
+        //    }
+        //    string Password = sb.ToString();
+        //    return Password;
+        //}
+        public string getRanTypeAndRan(int type,int length)
         {
             StringBuilder sb = new StringBuilder();
-            char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-            for (int i = 0; i < length; i++)
-            {
-                sb.Append(chars[this.Next(chars.Length - 1)]);
+            char[] chars="".ToCharArray();
+            switch(type){
+                case 1:
+                    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+                    break;
+                case 2:
+                    chars = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+                    break;
+                case 3:
+                    chars = "0123456789".ToCharArray();
+                    break;
             }
-            string Password = sb.ToString();
-            return Password;
-        }
-        public string GetRandomSendtoForth(int length)
-        {
-            StringBuilder sb = new StringBuilder();
-            char[] chars = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-            for (int i = 0; i < length; i++)
-            {
-                sb.Append(chars[this.Next(chars.Length - 1)]);
-            }
-            string Password = sb.ToString();
-            return Password;
-        }
-        public string GetRandomSendFinalNum(int length)
-        {
-            StringBuilder sb = new StringBuilder();
-            char[] chars = "0123456789".ToCharArray();
             for (int i = 0; i < length; i++)
             {
                 sb.Append(chars[this.Next(chars.Length - 1)]);
@@ -75,13 +95,14 @@ namespace randomPassword
             return Password;
         }
 
+
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = GetRandomFirstTittle(1) + GetRandomSendtoForth(3) + GetRandomSendFinalNum(4) + Environment.NewLine; 
-            textBox1.Text += GetRandomFirstTittle(1) + GetRandomSendtoForth(3) + GetRandomSendFinalNum(4) + Environment.NewLine; 
-            textBox1.Text += GetRandomFirstTittle(1) + GetRandomSendtoForth(3) + GetRandomSendFinalNum(4) + Environment.NewLine; 
-            textBox1.Text += GetRandomFirstTittle(1) + GetRandomSendtoForth(3) + GetRandomSendFinalNum(4) + Environment.NewLine;
-            textBox1.Text += GetRandomFirstTittle(1) + GetRandomSendtoForth(3) + GetRandomSendFinalNum(4);
+            textBox1.Text = getRanTypeAndRan(1, 1) + getRanTypeAndRan(2, 3) + getRanTypeAndRan(3,4) + Environment.NewLine;
+            textBox1.Text += getRanTypeAndRan(1, 1) + getRanTypeAndRan(2,3) + getRanTypeAndRan(3,4) + Environment.NewLine;
+            textBox1.Text += getRanTypeAndRan(1, 1) + getRanTypeAndRan(2,3) + getRanTypeAndRan(3,4) + Environment.NewLine;
+            textBox1.Text += getRanTypeAndRan(1, 1) + getRanTypeAndRan(2,3) + getRanTypeAndRan(3,4) + Environment.NewLine;
+            textBox1.Text += getRanTypeAndRan(1, 1) + getRanTypeAndRan(2,3) + getRanTypeAndRan(3,4);
 
         }
     }
